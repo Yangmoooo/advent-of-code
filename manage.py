155bc81655637a032551eb574b23044ed13e37d4
Yangmoooo <yangmoooo@outlook.com>
2024-12-10 13:20:27 +0800

    Improve usability

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import subprocess
 default_year = 2020
 
 
-def main():
+def arg_parser():
     parser = argparse.ArgumentParser(description="AoC Helper")
     subparsers = parser.add_subparsers(
         dest="command", help="Available commands", required=True
@@ -12,12 +12,14 @@ def main():
     year_msg = f"Year of the challenge, default is {default_year}"
     day_msg = "Day of the challenge"
 
-    # download 子命令
-    parser_download = subparsers.add_parser(
-        "download", aliases=["d"], help="Download the puzzle and input, need aoc-cli"
+    # fetch 子命令
+    parser_fetch = subparsers.add_parser(
+        "fetch",
+        aliases=["f"],
+        help="Download the input using aoc-cli. Prepare the src and Cargo.toml",
     )
-    parser_download.add_argument("year", nargs="?", type=int, help=year_msg)
-    parser_download.add_argument("day", type=int, help=day_msg)
+    parser_fetch.add_argument("year", nargs="?", type=int, help=year_msg)
+    parser_fetch.add_argument("day", type=int, help=day_msg)
 
     # run 子命令
     parser_run = subparsers.add_parser("run", aliases=["r"], help="Run the solution")
@@ -30,62 +32,106 @@ def main():
     parser_test.add_argument("year", nargs="?", type=int, help=year_msg)
     parser_test.add_argument("day", type=int, help=day_msg)
 
+    return parser
+
+
+def aoc_download(year, day, input_file):
+    download_cmd = [
+        "aoc",
+        "download",
+        "--year",
+        year,
+        "--day",
+        day,
+        "--input-only",
+        "--input-file",
+        input_file,
+    ]
+    subprocess.run(download_cmd)
+
+
+def create_src_file(src_file, year, day, input_file):
+    with open(src_file, "x") as f:
+        f.write(f"use aoc{year}::{{print_answer, read_file}};\n\n")
+        f.write("fn main() {\n")
+        f.write(f'    let input = read_file("{input_file}");\n')
+        f.write(f"    print_answer({day}, false, solve1(&input));\n")
+        f.write("}\n")
+
+
+def add_cargo_bin(cargo_toml, day):
+    with open(cargo_toml, "a") as f:
+        f.write(f'\n[[bin]]\nname = "day{day}"\npath = "src/day{day}.rs"\n')
+
+
+def aoc_fetch(year, day):
+    raw_day = day
+    day = "{:02d}".format(day)
+    input_file = f"aoc{year}/inputs/day{day}.txt"
+    aoc_download(year, day, input_file)
+    src_file = f"aoc{year}/src/day{day}.rs"
+    cargo_toml = f"aoc{year}/Cargo.toml"
+    create_src_file(src_file, year, raw_day, input_file)
+    add_cargo_bin(cargo_toml, day)
+
+
+def aoc_run(year, day, test_first):
+    run_cmd = [
+        "cargo",
+        "run",
+        "--package",
+        "aoc" + year,
+        "--bin",
+        "day" + day,
+    ]
+
+    if test_first:
+        test_cmd = [
+            "cargo",
+            "test",
+            "--package",
+            "aoc" + year,
+            "--bin",
+            "day" + day,
+        ]
+        exitcode = subprocess.run(test_cmd)
+        if exitcode.returncode == 0:
+            subprocess.run(run_cmd)
+    else:
+        subprocess.run(run_cmd)
+
+
+def aoc_test(year, day):
+    test_cmd = [
+        "cargo",
+        "test",
+        "--package",
+        "aoc" + year,
+        "--bin",
+        "day" + day,
+    ]
+    subprocess.run(test_cmd)
+
+
+def main():
+    parser = arg_parser()
     args = parser.parse_args()
 
     if args.year is None:
         args.year = default_year
 
-    day = "{:02d}".format(args.day)
-    name = "day" + day
     year = str(args.year)
-    pkg = "aoc" + year
-    subcommand = args.command
+    day = "{:02d}".format(args.day)
 
-    if subcommand == "download" or subcommand == "d":
-        puzzle_file = f"{pkg}/puzzles/{name}.md"
-        input_file = f"{pkg}/inputs/{name}.txt"
-        download_cmd = [
-            "aoc",
-            "download",
-            "--year",
-            year,
-            "--day",
-            day,
-            "--puzzle-file",
-            puzzle_file,
-            "--input-file",
-            input_file,
-        ]
-        subprocess.run(download_cmd)
-    else:
-        run_cmd = [
-            "cargo",
-            "run",
-            "--package",
-            pkg,
-            "--bin",
-            name,
-        ]
-        test_cmd = [
-            "cargo",
-            "test",
-            "--package",
-            pkg,
-            "--bin",
-            name,
-        ]
-        match args.command:
-            case "run" | "r":
-                if args.test:
-                    exitcode = subprocess.run(test_cmd)
-                    if exitcode.returncode == 0:
-                        subprocess.run(run_cmd)
-                else:
-                    subprocess.run(run_cmd)
-            case "test" | "t":
-                subprocess.run(test_cmd)
-            case _:
-                parser.print_help()
+    match args.command:
+        case "fetch" | "f":
+            aoc_fetch(year, args.day)
+        case "run" | "r":
+            aoc_run(year, day, args.test)
+        case "test" | "t":
+            aoc_test(year, day)
+        case _:
+            parser.print_help()
 
 
 if __name__ == "__main__":
