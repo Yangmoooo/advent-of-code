9d453e8813b7ba167b8d4711fe15b3da85e1aa77
Yangmoooo <yangmoooo@outlook.com>
2024-12-10 10:48:26 +0800

    Update AoC Helper

--- manage.py
+++ manage.py
@@ -6,26 +6,29 @@ default_year = 2020
 
 def main():
     parser = argparse.ArgumentParser(description="AoC Helper")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands", required=True
+    )
+    year_msg = f"Year of the challenge, default is {default_year}"
+    day_msg = "Day of the challenge"
 
     # download 子命令
     parser_download = subparsers.add_parser(
         "download", aliases=["d"], help="Download the puzzle and input, need aoc-cli"
     )
-    parser_download.add_argument(
-        "year", nargs="?", type=int, help="Year of the challenge"
-    )
-    parser_download.add_argument("day", type=int, help="Day of the challenge")
+    parser_download.add_argument("year", nargs="?", type=int, help=year_msg)
+    parser_download.add_argument("day", type=int, help=day_msg)
 
     # run 子命令
     parser_run = subparsers.add_parser("run", aliases=["r"], help="Run the solution")
-    parser_run.add_argument("year", nargs="?", type=int, help="Year of the challenge")
-    parser_run.add_argument("day", type=int, help="Day of the challenge")
+    parser_run.add_argument("year", nargs="?", type=int, help=year_msg)
+    parser_run.add_argument("day", type=int, help=day_msg)
+    parser_run.add_argument("-t", "--test", action="store_true", help="Test before run")
 
     # test 子命令
     parser_test = subparsers.add_parser("test", aliases=["t"], help="Test the solution")
-    parser_test.add_argument("year", nargs="?", type=int, help="Year of the challenge")
-    parser_test.add_argument("day", type=int, help="Day of the challenge")
+    parser_test.add_argument("year", nargs="?", type=int, help=year_msg)
+    parser_test.add_argument("day", type=int, help=day_msg)
 
     args = parser.parse_args()
 
@@ -73,11 +76,14 @@ def main():
         ]
         match args.command:
             case "run" | "r":
-                subprocess.run(run_cmd)
-            case "test" | "t":
-                exitcode = subprocess.run(test_cmd)
-                if exitcode.returncode == 0:
+                if args.test:
+                    exitcode = subprocess.run(test_cmd)
+                    if exitcode.returncode == 0:
+                        subprocess.run(run_cmd)
+                else:
                     subprocess.run(run_cmd)
+            case "test" | "t":
+                subprocess.run(test_cmd)
             case _:
                 parser.print_help()
 
