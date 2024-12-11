c068ae68b0c1e2bccec9fc05d5dbcab7c7882901
Yangmoooo <yangmoooo@outlook.com>
2024-12-11 17:01:50 +0800

    Use 'None' instead of 'No answer found'

--- aoc2020/src/lib.rs
+++ aoc2020/src/lib.rs
@@ -12,6 +12,6 @@ pub fn print_answer(day: u8, is_second: bool, answer: Option<usize>) {
     let part = if is_second { "Part2" } else { "Part1" };
     match answer {
         Some(answer) => println!("[AoC 2020 Day{day:02} {part}] {answer}"),
-        None => println!("[AoC 2020 Day{day:02} {part}] No answer found"),
+        None => println!("[AoC 2020 Day{day:02} {part}] None"),
     }
 }
